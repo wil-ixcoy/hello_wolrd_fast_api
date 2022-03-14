4fd959e45463be5b696b0e5a4148b46cd8d84ea8
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2022-03-13 22:29:03 -0600

    se aprende a usar path parameters pasandole parametros para que el envio de datos se adecue a lo que ncesitamos

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import Optional
 # pydantic para crear modelos
 from pydantic import BaseModel
 # fastapi
-from fastapi import Body, Query
+from fastapi import Body, Query, Path
 from fastapi import FastAPI
 
 # creamos una instancia de fastapi
@@ -16,7 +16,7 @@ class Persona(BaseModel):
     name: str
     last_name: str
     age: int
-    #valores opcionales y define que va a recibir si es que se envia
+    # valores opcionales y define que va a recibir si es que se envia
     hair_color: Optional[str] = None
     is_married: Optional[bool] = None
 
@@ -29,19 +29,48 @@ def home():
 
 
 @app.post("/person/new")
-#request body person: Persona
-
-#el triple punto dice que el parametro o atributo es obligatorio
+# request body person: Persona
+# el triple punto dice que el parametro o atributo es obligatorio
 def create_person(person: Persona = Body(...)):
     return person
 
-#validations query parameters
+# validations query parameters
 
-@app.get("/person/details")
-#se define un paramatro para el query en donde el nombre y la edad estan condicionados
-#por Query que limita el minimo y maximo de caracteres
+
+@app.get("/person/detail")
+# se define un paramatro para el query en donde el nombre y la edad estan condicionados
+# por Query que limita el minimo y maximo de caracteres
 def showPerson(
-    name :Optional[str] = Query(None, min_length=1, max_length=50),
-    age :Optional[str] = Query(None, min_length=1, max_length=3)
-    ):
+    name: Optional[str] = Query(
+        None,
+        min_length=1,
+        max_length=50,
+        title="Nombre de la persona",
+        description="este es el nombre de la persona, debe tener entre 1 y 50 caracteres"
+    ),
+    age: Optional[str] = Query(
+        None,
+        min_length=1,
+        max_length=3,
+        title="Edad de la persona",
+        description="este es la edad de la persona, debe tener entre 1 y 3 caracteres"
+    )
+):
     return {"name": name, "age": age}
+
+# validaciones path parameters
+
+#usamos get para obtener detalles de una persona por id
+@app.get("/person/detail/{person_id}")
+#definimos que debe el id a recibir debe ser mayor a 0 con gt=0, esta es una funcion que aplica
+#para query parameters y path parameters
+def showPerson(
+    person_id: int = Path(
+        ...,
+        gt=0,
+        title="Id de la persona",
+        despcriotion="este es el id de la persona, debe ser mayor a 0"
+    )
+):
+    #retornamos el id obtenido y mostramos que si existe 
+    return {person_id: "existe"}
