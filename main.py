03e95aa56f2873ae28a2e7bceb927076f715dd0d
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2022-03-13 19:44:42 -0600

    Se aprende request y responde body tambien se usa modelos con la libreria pydantic

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-''' imortamos fastapi '''
+# python
+from typing import Optional
+# pydantic para crear modelos
+from pydantic import BaseModel
+# fastapi
+from fastapi import Body
 from fastapi import FastAPI
 
-''' creamos una instancia de fastapi '''
+# creamos una instancia de fastapi
 app = FastAPI()
 
-''' usamos el path decorator y el path function para mostrar un mensaje '''
+# modelos
+
+
+class Persona(BaseModel):
+    name: str
+    last_name: str
+    age: int
+    #valores opcionales y define que va a recibir si es que se envia
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
+
 @app.get("/")
 def home():
-     return {"message": "Hello World"}
- 
+    return {"message": "Hello World"}
+
+# request and response body
+
+
+@app.post("/person/new")
+#request body person: Persona
+
+#el triple punto dice que el parametro o atributo es obligatorio
+def create_person(person: Persona = Body(...)):
+    return person
